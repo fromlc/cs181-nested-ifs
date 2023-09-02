35b87581a279a8f9076c20d4ef9fe8d67d834c02
L Carver <wifihum@gmail.com>
2023-09-02 13:14:46 -0700

    force upper case input, adjust output

--- gaddis9-Pr4-11-p158.cpp
+++ gaddis9-Pr4-11-p158.cpp
@@ -13,37 +13,40 @@ using namespace std;
 // entry point
 //------------------------------------------------------------------------------
 int main() {
-    char employed,    // Currently employed, Y or N
-        recentGrad;  // Recent graduate, Y or N
+    char employed;    // Currently employed, Y or N
+    char recentGrad;  // Recent graduate, Y or N
 
     // Is the user employed and a recent graduate?
-    cout << "\nLet's see if you qualify for a special interest rate!\n\n";
+    cout << "\nLet's see if you qualify for a";
+    cout << "\nspecial interest rate!\n\n";
 
-    cout << "Are you employed? (y/n)";
+    cout << "Are you currently employed? (y/n) ";
     cin >> employed;
+    employed = toupper(employed);       // FORCE UPPERCASE
 
-    cout << "Did you graduate from college\n";
-    cout << "in the past two years? (y/n)";
+    cout << "\nDid you graduate from college";
+    cout << "\nin the past two years ? (y/n) ";
     cin >> recentGrad;
+    recentGrad = toupper(recentGrad);   // FORCE UPPERCASE
 
-    // Determine the user's loan qualifications.
+    // Determine user's loan qualifications
     if (employed == 'Y')
     {
         if (recentGrad == 'Y') // Nested if
         {
-            cout << "You qualify for the special ";
-            cout << "interest rate.\n";
+            cout << "\nYou qualify for the special interest rate!\n";
         }
         else // Not a recent grad, but employed
         {
-            cout << "You must have graduated from ";
-            cout << "college in the past two\n";
-            cout << "years to qualify.\n";
+            cout << "\nSorry, you must have graduated";
+            cout << "\nfrom college in the past two";
+            cout << "\nyears to qualify.\n";
         }
     }
     else  // Not employed
     {
-        cout << "You must be employed to qualify.\n";
+        cout << "\nSorry, you must be currently employed";
+        cout << "\nto qualify.\n";
     }
     return 0;
 }
